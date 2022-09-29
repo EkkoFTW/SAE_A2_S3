109757d7d724062b9872943109e42680161a9ac5
EkkoFTW <theodem28@gmail.com>
2022-09-29 12:31:27 +0200

    DB 3

--- Messagerie/models.py
+++ Messagerie/models.py
@@ -1,66 +1,34 @@
 import base64
 from django.db import models
-
-class Message(models.Model):
-    ID_Message = models.IntegerField()
-    ID_Sender = models.IntegerField()
-    ID_Reply = models.IntegerField(default=0)
-    Text = models.CharField(max_length=5000)
-    ID_Conv = models.IntegerField()
-    Date = models.DateTimeField('date_published')
+from django.utils import timezone
 
 class Users(models.Model):
-    ID_User = models.IntegerField()
+    ID_User = models.BigAutoField(primary_key=True)
     Username = models.CharField(max_length=30)
     Password = models.CharField(max_length=30)
     PP = models.CharField(max_length=1000)
 
-class UserToConv(models.Model):
-    ID_User = models.IntegerField()
-    ID_Conv = models.TextField(db_column='data',blank=True)
-
-    def set_data(self, data):
-        self._data = base64.encodestring(data)
-
-    def get_data(self):
-         return base64.decodestring(self._data)
-
-    data = property(get_data, set_data)
-
-class ConvToUser(models.Model):
-    ID_Conv = models.IntegerField();
-    ID_User = models.TextField(db_column='data',blank=True)
-
-    def set_data(self, data):
-        self._data = base64.encodestring(data)
-
-    def get_data(self):
-         return base64.decodestring(self._data)
-
-    data = property(get_data, set_data)
+class Message(models.Model):
+    ID_Message = models.BigAutoField(primary_key=True)
+    ID_Sender = models.ForeignKey("Users", on_delete=models.DO_NOTHING)
+    ID_Reply = models.ForeignKey("self", on_delete=models.DO_NOTHING)
+    Text = models.CharField(max_length=5000)
+    ID_Conv = models.IntegerField()
+    Date = models.DateTimeField()
 
 class Conv_User(models.Model):
-    ID_Conv = models.IntegerField();
-    ID_User = models.TextField(db_column='data',blank=True)
+    ID_Conv = models.BigAutoField(primary_key=True)
+    ID_Message = models.ManyToManyField(Message)
     PrivateKey = models.IntegerField()
     Publickey = models.IntegerField()
-    def set_data(self, data):
-        self._data = base64.encodestring(data)
 
-    def get_data(self):
-         return base64.decodestring(self._data)
+class Conv_Admin(Conv_User):
+    FOR_NOW = models.IntegerField()
 
-    data = property(get_data, set_data)
+class UserToConv(models.Model):
+    ID_User = models.ForeignKey("Users", on_delete = models.DO_NOTHING)
+    ID_Conv = models.ManyToManyField(Conv_Admin)
 
-class Conv_Admin(models.Model):
-    ID_Conv = models.IntegerField()
-    ID_Message = models.TextField(db_column='data', blank=True)
-    PrivateKey = models.IntegerField()
-    Publickey = models.IntegerField()
-    def set_data(self, data):
-        self._data = base64.encodestring(data)
-
-    def get_data(self):
-         return base64.decodestring(self._data)
-
-    data = property(get_data, set_data)
+class ConvToUser(models.Model):
+    ID_Conv = models.ForeignKey("Conv_User", on_delete=models.DO_NOTHING)
+    ID_User = models.ManyToManyField(Users)
