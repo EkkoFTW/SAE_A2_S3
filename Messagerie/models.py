4751f72958aa860f825e148915361b8e33c48ef4
EkkoFTW <theodem28@gmail.com>
2022-09-30 17:39:37 +0200

    DB 4

--- Messagerie/models.py
+++ Messagerie/models.py
@@ -3,22 +3,25 @@ from django.db import models
 from django.utils import timezone
 
 class Users(models.Model):
-    ID_User = models.BigAutoField(primary_key=True)
     Username = models.CharField(max_length=30)
     Password = models.CharField(max_length=30)
     PP = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.Username
+
 class Message(models.Model):
-    ID_Message = models.BigAutoField(primary_key=True)
-    ID_Sender = models.ForeignKey("Users", on_delete=models.DO_NOTHING)
-    ID_Reply = models.ForeignKey("self", on_delete=models.DO_NOTHING)
+    ID_Sender = models.ForeignKey("Users", on_delete=models.DO_NOTHING, related_name="User_Sender")
+    ID_Receiver = models.ForeignKey("Users", on_delete=models.DO_NOTHING, related_name="User_Receiver")
+    ID_Reply = models.ForeignKey("self", on_delete=models.DO_NOTHING, null=True, blank=True)
     Text = models.CharField(max_length=5000)
     ID_Conv = models.IntegerField()
     Date = models.DateTimeField()
 
+    def __str__(self):
+        return "S: " + str(self.ID_Sender) + "  R:  " + str(self.ID_Receiver) + "   Texte: " + str(self.Text) + "   Time: " + str(self.Date)
+
 class Conv_User(models.Model):
-    ID_Conv = models.BigAutoField(primary_key=True)
-    ID_Message = models.ManyToManyField(Message)
     PrivateKey = models.IntegerField()
     Publickey = models.IntegerField()
 
