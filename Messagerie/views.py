590118b42d2b502cb0e7b1a50d0b74e56f04bf5d
EkkoFTW <theodem28@gmail.com>
2022-10-04 10:54:16 +0200

    webpage 1

--- Messagerie/views.py
+++ Messagerie/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from .models import *
 def index(request):
-    return HttpResponse("Hello, world. You're at the Messagerie index.")
+    latest_message_list = Message.objects.order_by('-Date')
+    template = loader.get_template('Messagerie/Index.html')
+    context = {'latest_message_list': latest_message_list, }
+    text = request.GET.get('text')
+    toAdd = Message(Text=text)
+    return HttpResponse(template.render(context, request))
+
+def log(request):
+    template = loader.get_template('Messagerie/Log.html')
+    latest_message_list = Message.objects.order_by('-Date')
+    context = {'latest_message_list': latest_message_list, }
+
+    return HttpResponse(template.render(context, request))
 
